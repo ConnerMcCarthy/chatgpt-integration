8f7c1fd3e40add678414ca3a1501a7bb09d94304
Conner McCarthy <conner.mccarthy.97@gmail.com>
2023-05-03 16:47:15 -0400

    Seperated keep and drive API access. Added chatGPT json integration

---
+++ driveGPT.py
@@ -0,0 +1,80 @@
+import io
+import os.path
+import json
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+import openai
+
+noteID = '1bX2GAEjz0u-Ea7FIsQ0CdvVtdy5AeOjaSJ5P9ZRq9oY1S3p1cSYY_k5fn-5VVw'
+api_key = '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'
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/drive']
+
+# The ID of a sample document.
+DOCUMENT_ID = '1blJ0DQ-GD6-jxBUh0hKrWsoySvKwInED'
+
+
+def main():
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build('drive', 'v3', credentials=creds)
+
+        # Call the Drive v3 API
+        results = service.files().list(
+            pageSize=10, fields="nextPageToken, files(id, name)").execute()
+        items = results.get('files', [])
+
+        notesZip = service.files().get(fileId = '1blJ0DQ-GD6-jxBUh0hKrWsoySvKwInED')
+        print(notesZip)
+
+        if not items:
+            print('No files found.')
+            return
+        print('Files:')
+        for item in items:
+            print(u'{0} ({1})'.format(item['name'], item['id']))
+    except HttpError as error:
+        # TODO(developer) - Handle errors from drive API.
+        print(f'An error occurred: {error}')
+
+    openai.api_key = api_key
+    prompt = 'Give one random word' #+ gnote.text
+    #response = ask(prompt)
+    #print(response)
+
+def ask(prompt):
+    response = openai.Completion.create(
+        engine = 'gpt-3.5-turbo',
+        prompt=prompt,
+        max_tokens=100,
+        n=1,
+        stop=None,
+        temperature=0.7
+    )
+    return response.choices[0].text.strip()
+
+if __name__ == '__main__':
+    main()
--- keepGPT.py
+++ keepGPT.py
@@ -1,34 +1,49 @@
+import io
 import os.path
+import json
 
 import openai
-import gkeepapi
 
-noteID = '1bX2GAEjz0u-Ea7FIsQ0CdvVtdy5AeOjaSJ5P9ZRq9oY1S3p1cSYY_k5fn-5VVw'
-api_key = 'key'
+api_key = ''
+
+folder_name = 'KeepNotes'
 
 def main():
-    keep = gkeepapi.Keep()
-    keep.login('mcconner97@gmail.com', 'password')
     
-    gnote = keep.get(noteID)
-    print(gnote.title)
-    print(gnote.text)
-
     openai.api_key = api_key
-    prompt = 'Repeat this message: ' + gnote.text
-    response = ask(prompt)
+    prompt = 'What are my notes about? Create a summary and title.'
+
+    json_notes = read_json(folder_name)
+
+    for note in json_notes:
+        print(note['title'])
+
+    notes = "\n".join(json.dumps(note) for note in json_notes)
+    response = ask(prompt, notes)
     print(response)
 
-def ask(prompt):
-    response = openai.Completion.create(
-        engine = 'text-davinci-003',
-        prompt=prompt,
-        max_tokens=100,
-        n=1,
-        stop=None,
-        temperature=0.7
+def read_json(notePath):
+    json_notes = []
+    count = 0
+    for filename in os.listdir(notePath):
+        if filename.endswith('.json') & (count < 2):
+            file_path = os.path.join(notePath,filename)
+            count += 1
+            with open(file_path) as file:
+                json_data = json.load(file)
+                json_notes.append(json_data)
+    return json_notes
+
+def ask(prompt, notes):
+    response = openai.ChatCompletion.create(
+        model = 'gpt-3.5-turbo',
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": "You have access to a list of notes with title, date, text content, and links. The notes are listed here as JSON files: " + notes},
+            {"role": "user", "content": prompt}
+        ]
     )
-    return response.choices[0].text
+    return response
 
 if __name__ == '__main__':
     main()
