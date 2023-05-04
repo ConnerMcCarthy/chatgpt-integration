28458debaa5ea860250262ec0c26893d6cb96e65
Conner McCarthy <conner.mccarthy.97@gmail.com>
2023-05-03 23:15:56 -0400

    adds working LLMs and compares the response to a prompt

--- langchain_test.py
+++ langchain_test.py
@@ -1,17 +1,24 @@
 import openai
-import langchain
+import os
+import tiktoken
 
 from langchain.llms import OpenAI
 
-davinci = OpenAI(model_name='text-davinci-003')
-
 def main():
-    
+
     api_path = "./secret.txt"
-    openai.api_key=read_api_key(api_path)
+    api_key = read_api_key(api_path)
+    llm = OpenAI(temperature=0.9, openai_api_key=api_key)
+    davinci = OpenAI(model_name='text-davinci-003', openai_api_key=api_key)
+    prompt = "Whats a good name for weather at the center of a galaxy?"
+    print(llm(prompt))
+    print(davinci(prompt))
 
 
 def read_api_key(api_path):
     with open(api_path, 'r') as file:
         api_key = file.read().strip()
     return api_key
+
+if __name__ == '__main__':
+    main()
